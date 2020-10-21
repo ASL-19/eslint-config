d50d169b19bb2b8d49f9df9423c34b5db1076d9b
Grant Heaslip <grant@asl19.org>
2020-10-21 12:26:39 -0400

    Upgrade dependencies, consolidate rules from internal projects
    * Use new @rushstack/eslint-config/profile/web-app profile and mixins.
    
    * Add @emotion/eslint-plugin rules to react config.
    
    * Organize rule overrides.

--- src/react.ts
+++ src/react.ts
@@ -1,11 +1,21 @@
 const eslintConfigReact = {
   extends: [
-    "@rushstack/eslint-config/react",
+    "@rushstack/eslint-config/mixins/react",
     "plugin:jsx-a11y/strict",
     "plugin:react-hooks/recommended",
     "prettier/react",
   ],
+  plugins: ["@emotion"],
   rules: {
+    // ------------------------------
+    // --- @emotion/eslint-plugin ---
+    // ------------------------------
+    "@emotion/jsx-import": "error",
+    "@emotion/syntax-preference": ["error", "string"],
+
+    // ------------------------------
+    // --- eslint-plugin-jsx-a11y ---
+    // ------------------------------
     // Via https://github.com/evcohen/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
     // Required until https://github.com/zeit/next.js/issues/5533 is resolved
     "jsx-a11y/anchor-is-valid": [
@@ -17,6 +27,10 @@ const eslintConfigReact = {
     ],
     // Deprecated and redundant due to jsx-a11y/label-has-associated-control
     "jsx-a11y/label-has-for": "off",
+
+    // ---------------------------
+    // --- eslint-plugin-react ---
+    // ---------------------------
     // Disabled until https://github.com/yannickcr/eslint-plugin-react/issues/1848 is fixed
     "react/jsx-one-expression-per-line": "off",
     "react/jsx-props-no-spreading": "off",
