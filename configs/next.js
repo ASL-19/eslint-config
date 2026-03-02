f6128db23eacc29170a781f2b5094152b22b5b97
Min Van <min@asl19.org>
2026-03-02 10:57:34 +0700

    Include a description after the "@ts-expect-error"

--- configs/next.js
+++ configs/next.js
@@ -4,10 +4,10 @@ import { defineConfig } from "eslint/config";
 const nextConfig = defineConfig([
   {
     plugins: {
-      // @ts-expect-error
+      // @ts-expect-error - Next.js plugin types don't perfectly match ESLint flat config Plugin type
       "@next/next": next,
     },
-    // @ts-expect-error
+    // @ts-expect-error - Rule types from Next.js plugin don't perfectly match ESLint RulesConfig
     rules: {
       ...next.configs.recommended.rules,
       "@next/next/no-html-link-for-pages": "off",
